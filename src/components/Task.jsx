86542a96cc54b1297839f6fd0e07f2394894395e
Ador Shahriar <ador.shahriar@gmail.com>
2023-12-22 11:28:58 +0600

    complete styling updated

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -36,7 +36,7 @@ const Task = ({ task, handleRemove, handleUpdate, statusChange }) => {
         <p className="font-bold text-lg">{task.title}</p>
         <p className="font-medium">{task.description}</p>
         <div className="flex justify-start items-center gap-2">
-          <p className={`font-medium ${remaining <= 2 && `text-red-600`} ${remaining < 4 && remaining > 2 && `text-yellow-600`} ${remaining > 4 && `text-green-600`} ${task.status === 'complete' && 'text-black'}`}>
+          <p className={`font-medium ${task.status !== 'complete' && remaining <= 2 && `text-red-600`} ${task.status !== 'complete' && remaining < 4 && remaining > 2 && `text-yellow-600`} ${task.status !== 'complete' && remaining > 4 && `text-green-600`} ${task.status === 'complete' && 'text-black'}`}>
             Deadline: {task.deadline} (
             {remaining > 0 ? remaining : -(remaining)}{" "}
             {`${remaining >= 0 && remaining <= 1 ? 'day left' : ''}`}{`${remaining > 1 ? 'days left' : ''}`}{`${remaining < 0 && remaining >= -1 ? 'day passed' : ''}`}{`${remaining <-1 ? 'days passed' : ''}`}){" "}
